183c0b08a96ff0af912f9a4b9485f522cc305b73
B-2017 <bobousenselemani2021@gmail.com>
2025-09-22 08:45:52 +0200

    Slet gamle kommentarer

--- Loanity.Web/Controllers/Crud/UserController.cs
+++ Loanity.Web/Controllers/Crud/UserController.cs
@@ -1,99 +1,9 @@
-﻿//using Loanity.Domain.Dtos;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.AspNetCore.Mvc.Rendering;
-
-//namespace Loanity.Web.Controllers.Crud
-//{
-//    public class UserController : Controller
-//    {
-//        private readonly IHttpClientFactory _http;
-
-//        public UserController(IHttpClientFactory http) => _http = http;
-
-//        private async Task LoadRolesAsync()
-//        {
-//            var client = _http.CreateClient("LoanityApi");
-//            var roles = await client.GetFromJsonAsync<List<RoleDto>>("api/role");
-//            ViewBag.Roles = new SelectList(roles, "Id", "Name");
-//        }
-
-//        public record RoleDto(int Id, string Name);
-
-
-//        public async Task<IActionResult> Read()
-//        {
-//            var client = _http.CreateClient("LoanityApi");
-//            var users = await client.GetFromJsonAsync<List<UserDto>>("api/user");
-
-//            return View(users ?? new());
-//        }
-
-//        public async Task<IActionResult> Create()
-//        {
-//            await LoadRolesAsync();
-//            return View("Create");
-//        }
-
-
-//        [HttpPost]
-//        public async Task<IActionResult> Create(UserDto user)
-//        {
-//            var client = _http.CreateClient("LoanityApi");
-//            var response = await client.PostAsJsonAsync("api/user", user);
-
-//            if (response.IsSuccessStatusCode)
-//                return RedirectToAction("Read");
-
-//            await LoadRolesAsync();
-//            return View();
-//        }
-
-
-//        public async Task<IActionResult> Update(int id)
-//        {
-//            var client = _http.CreateClient("LoanityApi");
-//            var user = await client.GetFromJsonAsync<UserDto>($"api/user/{id}");
-//            await LoadRolesAsync();
-//            return View(user);
-//        }
-
-
-//        [HttpPost]
-//        public async Task<IActionResult> Update(int id, UserDto user)
-//        {
-//            user = user with { Id = id };
-//            var client = _http.CreateClient("LoanityApi");
-//            var response = await client.PutAsJsonAsync($"api/user/{id}", user);
-
-//            if (response.IsSuccessStatusCode)
-//                return RedirectToAction("Read");
-
-//            await LoadRolesAsync();
-//            return View("Update", user);
-//        }
-
-
-//        public async Task<IActionResult> Delete(int id)
-//        {
-//            var client = _http.CreateClient("LoanityApi");
-//            var user = await client.GetFromJsonAsync<UserDto>($"api/user/{id}");
-//            return View(user);
-//        }
-
-//        [HttpPost, ActionName("Delete")]
-//        public async Task<IActionResult> DeleteConfirmed(int id)
-//        {
-//            var client = _http.CreateClient("LoanityApi");
-//            await client.DeleteAsync($"api/user/{id}");
-//            return RedirectToAction("Read");
-//        }
-//    }
-
-//}
+﻿
 using Loanity.Web.Controllers.Common;
 using Loanity.Domain.Dtos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Loanity.Domain.Entities;
 
 namespace Loanity.Web.Controllers;
 
@@ -104,11 +14,11 @@ public class UserController : CrudControllerWeb<UserDto>
 
     private async Task LoadRolesAsync()
     {
-        var roles = await _http.GetFromJsonAsync<List<RoleDto>>("api/role");
+        var roles = await _http.GetFromJsonAsync<List<Role>>("api/role");
         ViewBag.Roles = new SelectList(roles, "Id", "Name");
     }
 
-    public record RoleDto(int Id, string Name);
+    //public record RoleDto(int Id, string Name);
 
     public override async Task<IActionResult> Create()
     {
